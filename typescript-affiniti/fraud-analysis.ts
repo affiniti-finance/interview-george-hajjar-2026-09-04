c6969162e1a78e5c802b45280c5a13b49a8971ae
George Hajjar <georgehajjar99@gmail.com>
2026-09-04 14:09:17 -0400

    end of interview

--- typescript-affiniti/fraud-analysis.ts
+++ typescript-affiniti/fraud-analysis.ts
@@ -1,5 +1,4 @@
-
-import testTransactionsData from "./test-transactions.json";
+import testTransactionsData from './test-transactions.json';
 
 /**
  * A Transaction represents a card trying to spend money
@@ -7,22 +6,25 @@ import testTransactionsData from "./test-transactions.json";
 type Transaction = {
   id: string;
   cardNumber: string; // 16-digit PAN
-  amount: number;     // amount in cents
+  amount: number; // amount in cents
   country: string;
-  timestamp: string;  // ISO timestamp, for example "2025-03-13T09:00:00.000Z",
+  timestamp: string; // ISO timestamp, for example "2025-03-13T09:00:00.000Z",
   category: string;
 };
 
 // These are the only fraud rules to support for today
-type FraudRule = "Unusual Amount" | "Unusual Location" | "Suspicious Category Sequence"
+type FraudRule =
+  | 'Unusual Amount'
+  | 'Unusual Location'
+  | 'Suspicious Category Sequence';
 
 /**
  * A FraudScore captures the transaction decision, the card status, a risk score,
  * and the rules that contributed to the FraudScore
  */
 type FraudScore = {
-  decision: "ACCEPTED" | "DECLINED";
-  cardStatus: "MONITORING" | "AT_RISK";
+  decision: 'ACCEPTED' | 'DECLINED';
+  cardStatus: 'MONITORING' | 'AT_RISK';
   fraudRiskScore: number;
   rulesTriggered: FraudRule[];
 };
@@ -31,30 +33,39 @@ type FraudScore = {
  * The TestTransaction data is simply a Transaction with the expected FraudScore in the "expected" property
  */
 type TestTransaction = Transaction & {
-  expected: FraudScore
-}
+  expected: FraudScore;
+};
 
 /**
  * Determines if a FraudScore is correct
  * @param actual the fraud score as calculated by the code
  * @param expected the expected fraud score from the test transaction data
  */
-function checkFraudScore (actual: FraudScore, expected: FraudScore): string[] {
+function checkFraudScore(actual: FraudScore, expected: FraudScore): string[] {
   const incorrect: string[] = [];
   if (actual.decision !== expected.decision) {
-    incorrect.push("decision");
+    incorrect.push('decision');
   }
   if (actual.cardStatus !== expected.cardStatus) {
-    incorrect.push("cardStatus");
+    incorrect.push('cardStatus');
   }
   if (actual.fraudRiskScore !== expected.fraudRiskScore) {
-    incorrect.push("fraudRiskScore");
+    incorrect.push('fraudRiskScore');
   }
-  if (JSON.stringify([...actual.rulesTriggered].sort()) !== JSON.stringify([...expected.rulesTriggered].sort())) {
-    incorrect.push("rulesTriggered");
+  if (
+    JSON.stringify([...actual.rulesTriggered].sort()) !==
+    JSON.stringify([...expected.rulesTriggered].sort())
+  ) {
+    incorrect.push('rulesTriggered');
   }
   return incorrect;
 }
+
+// function checkCountry(transaction: Transaction): FraudScore {}
+
+// 5 tx -> 6 tx (drop last one)
+// Can, Us, Mex, Us, Brazil, Japan ->
+// Us, Us, Us, Kaza, Us, Us, Us, Us, Us, Us ->
 
 /**
  * Calculate a FraudScore for a transaction.
@@ -83,14 +94,38 @@ function checkFraudScore (actual: FraudScore, expected: FraudScore): string[] {
  * @param transaction a Transaction object
  * @return a FraudScore for the transaction
  */
-function calculateTransactionFraudScore(transaction: Transaction): FraudScore {
-  // TODO: This placeholder return value allows every transaction
-  // TODO: Replace with code to enforce the fraud rules described above
+function calculateTransactionFraudScore(
+  transaction: Transaction,
+  lastFiveCountries: string[],
+  transactionAverage: number,
+): FraudScore {
+  let transactionDecision: FraudScore['decision'] = 'ACCEPTED';
+  let transactionCardStatus: FraudScore['cardStatus'] = 'MONITORING';
+  let transactionFraudRiskScore: FraudScore['fraudRiskScore'] = 0;
+  let transactionRulesTriggered: FraudScore['rulesTriggered'] = [];
+
+  // Case 1
+  const country = transaction.country;
+  if (!lastFiveCountries.some((item) => item === transaction.country)) {
+    transactionDecision = 'DECLINED';
+    transactionFraudRiskScore += 51;
+    transactionRulesTriggered.push('Unusual Location');
+  }
+
+  // Case 2
+  if (transactionAverage * 2 >= transaction.amount) {
+    transactionFraudRiskScore += 20;
+  }
+
+  // Case 3
+
+  //  *     Then ==> +32 Fraud Risk Score
+
   return {
-    decision: "ACCEPTED",
-    cardStatus: "MONITORING",
-    fraudRiskScore: 0,
-    rulesTriggered: [],
+    decision: transactionDecision,
+    cardStatus: transactionCardStatus,
+    fraudRiskScore: transactionFraudRiskScore,
+    rulesTriggered: transactionRulesTriggered,
   };
 }
 
@@ -99,19 +134,54 @@ function calculateTransactionFraudScore(transaction: Transaction): FraudScore {
  * @param transactions the transactions to evaluate
  */
 function main(transactions: TestTransaction[]) {
+  let lastFiveCountries: string[] = [];
+
+  let lastAverageTxAmount: number | undefined = undefined;
+
+  const suspiciousSequences: Record<string, string> = {
+    atm: 'jewelry',
+    online_gaming: 'luxury_goods',
+    electronics: 'pawn_shop',
+  };
+
+  let lastCategory: string = '';
+
   for (const transaction of transactions) {
-    const fraudScore = calculateTransactionFraudScore(transaction);
+    // Last 5 country logic
+    lastFiveCountries.push(transaction.country);
+    lastFiveCountries = lastFiveCountries.slice(-5);
+
+    if (lastAverageTxAmount) {
+      lastAverageTxAmount = (lastAverageTxAmount + transaction.amount) / 2;
+    } else {
+      lastAverageTxAmount = transaction.amount;
+    }
+
+    // Keep track of the last category from the previous tx
+    // use Record above (suspiciousSequences) to check if current tx is value (with key:value record)
+    //  return boolean to calculateTransactionFraudScore to say if we should +32
+
+    const fraudScore = calculateTransactionFraudScore(
+      transaction,
+      lastFiveCountries,
+      lastAverageTxAmount,
+    );
     const incorrectFields = checkFraudScore(fraudScore, transaction.expected);
     if (incorrectFields.length > 0) {
-      console.log(`Transaction ${transaction.id} has errors: ${incorrectFields.join(", ")}`);
+      console.log(
+        `Transaction ${transaction.id} has errors: ${incorrectFields.join(', ')}`,
+      );
     } else {
       console.log(`Transaction ${transaction.id} has correct fraud score`);
     }
   }
+
+  console.log(lastFiveCountries);
 }
 
 // Load the transactions
-const testTransactions: TestTransaction[] = testTransactionsData as TestTransaction[];
+const testTransactions: TestTransaction[] =
+  testTransactionsData as TestTransaction[];
 
 // Evaluate fraud scores
 main(testTransactions);
